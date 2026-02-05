a9b64f2926f07752734844c3db4da225e7e5e68e
LorentzaZweena <ariva02zweena@gmail.com>
2026-02-05 09:26:40 +0700

    Create lingkaran.java

---
+++ lingkaran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author User
+ */
+public class lingkaran {
+    public static void main(String args[]) {
+    double pi, r, a;
+    r = 10.8888; // jadi – jari lingkaran
+    pi = 3.1415926; // pi
+    a = pi * r * r;
+    System.out.println("Luas Lingkaran adalah " + a);
+ }
+
+}
